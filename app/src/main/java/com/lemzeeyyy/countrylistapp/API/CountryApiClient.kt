e641fd61809e3dc654a7cc2bcb94d878ab5adb61
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-11-11 15:07:38 +0100

    app executor (background)

--- app/src/main/java/com/lemzeeyyy/countrylistapp/API/CountryApiClient.kt
+++ app/src/main/java/com/lemzeeyyy/countrylistapp/API/CountryApiClient.kt
@@ -1,10 +1,84 @@
 package com.lemzeeyyy.countrylistapp.API
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.lemzeeyyy.countrylistapp.Service
 import com.lemzeeyyy.countrylistapp.model.CountryResponse
+import retrofit2.Call
+import retrofit2.Response
+import java.io.IOException
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 class CountryApiClient {
-    private val countryObject: MutableLiveData<List<CountryResponse>>? = null
+     private lateinit var countryObject: MutableLiveData<List<CountryResponse>?>
 
-    private val instance: CountryApiClient? = null
+    private var instance: CountryApiClient? = null
+
+    private var retrieveCountryRunnable: RetrieveCountryRunnable? = null
+
+    constructor() {
+        this.countryObject = countryObject
+    }
+
+    fun getInstance(): CountryApiClient? {
+        if (instance == null) {
+           instance = CountryApiClient()
+        }
+        return instance
+    }
+
+    fun getCountries(): MutableLiveData<List<CountryResponse>?> {
+        return countryObject
+    }
+
+
+    fun searchCountryApi(query: String?) {
+        if (retrieveCountryRunnable != null) {
+            retrieveCountryRunnable = null
+        }
+        retrieveCountryRunnable = RetrieveCountryRunnable(query!!)
+        val myHandler: Future<*> =
+            AppExecutor.getInstance().networkIO()!!.submit(retrieveCountryRunnable)
+        AppExecutor.getInstance().networkIO()!!.schedule(Runnable { //Canceling the retrofit call
+            myHandler.cancel(true)
+        }, 5000, TimeUnit.MILLISECONDS)
+    }
+
+    private inner class RetrieveCountryRunnable() : Runnable{
+
+        lateinit var  query: String
+         var cancelRequest: Boolean=false
+
+        constructor(query: String) : this() {
+            this.query = query
+            cancelRequest = false;
+        }
+
+
+        override fun run() {
+            try {
+                val response: Response<*> = getCountries(query)!!.execute()
+                if (cancelRequest) {
+                    return
+                }
+        }catch (e : IOException){
+            e.printStackTrace()
+                countryObject.postValue(null)
+
+        }
+
+        }
+        fun getCountries(query: String): Call<List<CountryResponse>>? {
+            return Service.getCountryApi()?.searchCountries(query);
+        }
+
+        private fun setCancelRequest() {
+            Log.d("CancelReq", "setCancelRequest: Cancelling Search Request")
+            cancelRequest = true
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/lemzeeyyy/countrylistapp/Service.kt
+++ app/src/main/java/com/lemzeeyyy/countrylistapp/Service.kt
@@ -7,14 +7,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Service {
-    private val retrofitBuilder: Retrofit.Builder = Retrofit.Builder()
-        .baseUrl(Credentials.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
 
-    private val retrofit = retrofitBuilder.build()
-    private val countryApi: CountryApi = retrofit.create(CountryApi::class.java)
+   companion object{
+       private val retrofitBuilder: Retrofit.Builder = Retrofit.Builder()
+           .baseUrl(Credentials.BASE_URL)
+           .addConverterFactory(GsonConverterFactory.create())
 
-    fun getCountryApi(): CountryApi? {
-        return countryApi
-    }
+       private val retrofit = retrofitBuilder.build()
+
+       private val countryApi: CountryApi = retrofit.create(CountryApi::class.java)
+       fun getCountryApi(): CountryApi? {
+           return countryApi
+       }
+   }
 }
